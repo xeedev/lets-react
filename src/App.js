79ef605766858e128cccf811ff0e0b8ffd08e121
xeemax <xeemax2.0@gmail.com>
2022-09-13 23:41:18 +0500

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import {TemporaryDrawer} from './components'
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TemporaryDrawer />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes />
     </div>
   );
 }
